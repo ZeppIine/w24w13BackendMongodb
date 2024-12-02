34fbde16fd3f1db7a82e9600bab056ca9aa5f687
ZeppIine <kim0904min@gmail.com>
2024-12-02 16:30:45 +0900

    Initial commit

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w13BackendMongodb/W24w13BackendMongodbApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20180147.w24w13BackendMongodb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w13BackendMongodbApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w13BackendMongodbApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/ce/s20180147/w24w13BackendMongodb/W24w13BackendMongodbApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20180147.w24w13BackendMongodb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w13BackendMongodbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
